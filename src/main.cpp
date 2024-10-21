7b0f96e78967635633d4eb3978bbd8f2849c0ef5
Jetson Kelso <jetogoo@gmail.com>
2024-10-20 23:15:04 -0600

    Class, enum, and random #

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+enum gameState {
+    WON, LOST, PLAYING
+};
+
+class Game {
+private:
+    string name;
+    int guesses;
+    int ranNumber;
+
+public:
+      // Constructor that takes user input
+    Game() {
+        cout << "Enter name: ";
+        cin >> name;
+
+        guesses = 0;
+        srand(time(NULL));
+        ranNumber = (rand() % 100) + 1;
+    }
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Guesses: " << guesses << endl;
+        cout << ranNumber << endl;
+    }
+
+
+};
 
 int main(){
-    std::cout << "Hello world" << std::endl;
+    cout << "Hello world" << endl;
+    Game player;
+
+    player.display();
+
     return 0;
 }
