15faf04e31f0f0b2ea665dfc42f52eb3e1c3d49b
Jetson Kelso <jetogoo@gmail.com>
2024-10-21 00:54:27 -0600

    Complete

--- src/main.cpp
+++ src/main.cpp
@@ -4,41 +4,78 @@
 
 using namespace std;
 
-enum gameState {
-    WON, LOST, PLAYING
+struct gameConfig{
+  string myName;
+  int maxNum;
+  int minNum;
 };
 
 class Game {
 private:
-    string name;
     int guesses;
     int ranNumber;
+    gameConfig config;
 
 public:
-      // Constructor that takes user input
-    Game() {
-        cout << "Enter name: ";
-        cin >> name;
+    Game(gameConfig gameConfig) {
+       
 
+        config = gameConfig;
         guesses = 0;
         srand(time(NULL));
-        ranNumber = (rand() % 100) + 1;
+        ranNumber = (rand() % config.maxNum) + config.minNum;
     }
 
+
     void display() {
-        cout << "Name: " << name << endl;
         cout << "Guesses: " << guesses << endl;
         cout << ranNumber << endl;
+    }
+
+    void playGame() {
+        int guess;
+
+        cout << "Welcome to the Guessing Game " << config.myName << "!" << endl;
+        cout << "You have 10 guesses" << endl;
+
+        do {
+            cout << "Enter your guess (1-100): ";
+            cin >> guess;
+            while (guess > config.maxNum || guess < config.minNum){
+                cout << "Incorrect input, try agian." << endl;
+                cin >> guess;
+            }
+            guesses++;
+
+            if (guess < ranNumber) {
+                cout << "Too low!" << endl;
+            } else if (guess > ranNumber) {
+                cout << "Too high!" << endl;
+            }
+        } while (guess != ranNumber && guesses < 10);
+        if (guess == ranNumber){
+            cout << "Congrats " << config.myName << ", you guessed the number " << ranNumber << " in " << guesses << " attempts!" << endl;
+        } else if (guesses <= 10){
+            cout << "Sorry " << config.myName << ", you ran out of guesses! The number was " << ranNumber << endl;
+        }
+
     }
 
 
 };
 
 int main(){
-    cout << "Hello world" << endl;
-    Game player;
+    gameConfig config;
+    config.minNum = 1;
+    config.maxNum = 100;
 
-    player.display();
+    cout << "Enter your name: ";
+    cin >> config.myName;
 
+    Game player(config);
+   
+    player.playGame();
+
+   
     return 0;
 }
